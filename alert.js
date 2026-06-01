ce49ac8fea58ffb96408370ffe023b3b84e0a6d5
Nanna <cph-nn238@cphbusiness.dk>
2026-06-01 21:34:13 +0200

    Update alert.js

--- alert.js
+++ alert.js
@@ -7,3 +7,16 @@ jQuery(document).ready(function($) {
         }
     });
 });
+
+// Skrift mellem card of wrapper på smagssiden
+const cards = document.querySelectorAll(".flavor-card");
+
+      cards.forEach((card) => {
+        card.addEventListener("click", () => {
+          cards.forEach((c) => {
+            c.classList.remove("active");
+          });
+
+          card.classList.add("active");
+        });
+      });
